5b7624c180f2007bb3fc0fdadc533bf53174f786
Daniel Martin <77379766+python3xdev@users.noreply.github.com>
2023-01-30 08:50:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from random import choice
+
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+	print(f"---<= Logged In As: {bot.user} =>---")
+
+
+@bot.event
+async def on_member_join(member):
+	channel = bot.get_channel(1065348602123259965)
+	message_versions = ['your journey has just begun', 'we hope you enjoy your stay', 'we know that your brilliance will contribute greatly to the growth of our community', 'we hope you’ll grow with us']
+	embed = discord.Embed(
+		title=f"🎉 Member.add() 🎉",
+		description=f"Welcome {member.mention}, {choice(message_versions)}!",
+		color=discord.Colour.green()
+	)
+	await channel.send(embed=embed)
+
+
+@bot.event
+async def on_member_remove(member):
+	channel = bot.get_channel(1066048757742968932)
+	message_versions = ['gave up on their journey', 'was yeeted and defeated (╯°□°）╯︵ ┻━┻', 'is no longer with us ', 'just gave up ¯\\_(ツ)_/¯']
+	embed = discord.Embed(
+		title=f"☹ Member.remove() ☹",
+		description=f"{member.mention} {choice(message_versions)}.",
+		color=discord.Colour.red()
+	)
+	await channel.send(embed=embed)
+
+
+bot.run('MTA2NzU3NDc0NTkyNDY1MzE0Ng.Ge5YSf.q9_sXYRzKPHLLZVJxehBsNNkRZ2Oh42JGuFVl8')
